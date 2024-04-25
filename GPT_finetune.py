ec5b80ca7a0f99e10b55e23a97d09020d5313c08
Yavuz Selim <96388835+selimvuz@users.noreply.github.com>
2024-04-25 07:55:35 +0300

    GPT-large fine-tune

--- GPT_finetune.py
+++ GPT_finetune.py
@@ -4,9 +4,9 @@ from datasets import Dataset
 from sklearn.model_selection import train_test_split
 from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments
 
-tokenizer = AutoTokenizer.from_pretrained("ytu-ce-cosmos/turkish-gpt2")
+tokenizer = AutoTokenizer.from_pretrained("ytu-ce-cosmos/turkish-gpt2-large")
 model = AutoModelForCausalLM.from_pretrained(
-    "ytu-ce-cosmos/turkish-gpt2", return_dict=True)
+    "ytu-ce-cosmos/turkish-gpt2-large", return_dict=True)
 
 if tokenizer.pad_token is None:
     tokenizer.pad_token = tokenizer.eos_token
@@ -15,7 +15,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
 # CSV dosyasını yükle ve sütunları birleştir
-data = pd.read_csv("datasets/instructions.csv")
+data = pd.read_csv("datasets/instructions_demo.csv")
 data['question'] = data['talimat'] + " " + \
     data['giriş'] + tokenizer.eos_token + data['çıktı']
 data = data.dropna(subset=['question', 'çıktı'])  # Eksik değerleri temizle
@@ -28,9 +28,13 @@ train_dataset = Dataset.from_pandas(train_data)
 eval_dataset = Dataset.from_pandas(eval_data)
 
 # Eğitim ve değerlendirme sırasında verileri de GPU'ya taşı
+
+
 def tokenize_function(examples):
-    combined_text = [(q + " " + g + tokenizer.eos_token + c) for q, g, c in zip(examples['talimat'], examples['giriş'], examples['çıktı'])]
-    inputs = tokenizer(combined_text, truncation=True, max_length=512, padding="max_length", return_tensors="pt")
+    combined_text = [(q + " " + g + tokenizer.eos_token + c) for q, g,
+                     c in zip(examples['talimat'], examples['giriş'], examples['çıktı'])]
+    inputs = tokenizer(combined_text, truncation=True,
+                       max_length=512, padding="max_length", return_tensors="pt")
     inputs['labels'] = inputs.input_ids.detach().clone()
     eos_mask = inputs.input_ids == tokenizer.eos_token_id
     for i in range(inputs.input_ids.shape[0]):
@@ -39,7 +43,7 @@ def tokenize_function(examples):
             first_eos_idx = eos_indices[0]
             inputs['labels'][i][:first_eos_idx+1] = -100
         else:
-            inputs['labels'][i][:] = -100 
+            inputs['labels'][i][:] = -100
     return inputs
 
 
@@ -49,7 +53,7 @@ tokenized_eval_datasets = eval_dataset.map(tokenize_function, batched=True)
 training_args = TrainingArguments(
     output_dir='./results',
     num_train_epochs=3,
-    gradient_accumulation_steps=2,  # ?
+    gradient_accumulation_steps=2,
     per_device_train_batch_size=2,
     per_device_eval_batch_size=2,
     fp16=True,
@@ -71,8 +75,8 @@ trainer = Trainer(
     train_dataset=tokenized_train_datasets,
     eval_dataset=tokenized_eval_datasets,
     tokenizer=tokenizer
-)
+)   
 
 trainer.train()
 
-model.save_pretrained("./finetuned_gpt")
+model.save_pretrained("./finetuned_gpt_large")
